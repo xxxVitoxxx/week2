c27ee4cbdd4781da58bd7c99ee0bce1f6f2e65f9
xxxVitoxxx <bird06291994@gmail.com>
2024-08-10 00:48:08 +0800

    feat: use solidity to implement a Merkle tree

--- Part1/contracts/MerkleTree.sol
+++ Part1/contracts/MerkleTree.sol
@@ -4,26 +4,39 @@ pragma solidity ^0.8.0;
 import { PoseidonT3 } from "./Poseidon.sol"; //an existing library to perform Poseidon hash on solidity
 import "./verifier.sol"; //inherits with the MerkleTreeInclusionProof verifier contract
 
-contract MerkleTree is Verifier {
+contract MerkleTree is Groth16Verifier {
     uint256[] public hashes; // the Merkle tree in flattened array form
     uint256 public index = 0; // the current index of the first unfilled leaf
     uint256 public root; // the current Merkle root
 
     constructor() {
-        // [assignment] initialize a Merkle tree of 8 with blank leaves
+        hashes = new uint256[](2**3-1);
+        _updateRootHash();
     }
 
     function insertLeaf(uint256 hashedLeaf) public returns (uint256) {
-        // [assignment] insert a hashed leaf into the Merkle tree
+        // insert a hashed leaf into the Merkle tree
+        require(index < 8, "Merkle tree is full");
+        hashes[index++] = hashedLeaf;
+        _updateRootHash();
+        return root;
     }
 
+    // verify an inclusion proof and check that the proof root matches current root
     function verify(
-            uint[2] memory a,
-            uint[2][2] memory b,
-            uint[2] memory c,
-            uint[1] memory input
+            uint[2] calldata a,
+            uint[2][2] calldata b,
+            uint[2] calldata c,
+            uint[1] calldata input
         ) public view returns (bool) {
+        require(input[0] == root, "Proof root does not match current root");
+        return verifyProof(a, b, c, input);
+    }
 
-        // [assignment] verify an inclusion proof and check that the proof root matches current root
+    function _updateRootHash() internal {
+        for (uint256 i = 8; i < 15; ++i) {
+            hashes[i] = PoseidonT3.poseidon([hashes[2*i - 16], hashes[2*i -15]]);
+        }
+        root = hashes[14];
     }
 }
--- Part1/test/merkle-test.js
+++ Part1/test/merkle-test.js
@@ -4,51 +4,60 @@ const { ethers } = require("hardhat");
 const { groth16 } = require("snarkjs");
 
 describe("MerkleTree", function () {
-    let merkleTree;
+  let merkleTree;
 
-    beforeEach(async function () {
+  beforeEach(async function () {
+    const PoseidonT3 = await ethers.getContractFactory(
+      poseidonContract.generateABI(2),
+      poseidonContract.createCode(2)
+    );
+    const poseidonT3 = await PoseidonT3.deploy();
+    await poseidonT3.deployed();
 
-        const PoseidonT3 = await ethers.getContractFactory(
-            poseidonContract.generateABI(2),
-            poseidonContract.createCode(2)
-        )
-        const poseidonT3 = await PoseidonT3.deploy();
-        await poseidonT3.deployed();
-
-        const MerkleTree = await ethers.getContractFactory("MerkleTree", {
-            libraries: {
-                PoseidonT3: poseidonT3.address
-            },
-          });
-        merkleTree = await MerkleTree.deploy();
-        await merkleTree.deployed();
+    const MerkleTree = await ethers.getContractFactory("MerkleTree", {
+      libraries: {
+        PoseidonT3: poseidonT3.address,
+      },
     });
+    merkleTree = await MerkleTree.deploy();
+    await merkleTree.deployed();
+  });
 
-    it("Insert two new leaves and verify the first leaf in an inclusion proof", async function () {
-        await merkleTree.insertLeaf(1);
-        await merkleTree.insertLeaf(2);
+  it("Insert two new leaves and verify the first leaf in an inclusion proof", async function () {
+    await merkleTree.insertLeaf(1);
+    await merkleTree.insertLeaf(2);
 
-        const node9 = (await merkleTree.hashes(9)).toString();
-        const node13 = (await merkleTree.hashes(13)).toString();
+    const node9 = (await merkleTree.hashes(9)).toString();
+    const node13 = (await merkleTree.hashes(13)).toString();
 
-        const Input = {
-            "leaf": "1",
-            "path_elements": ["2", node9, node13],
-            "path_index": ["0", "0", "0"]
-        }
-        const { proof, publicSignals } = await groth16.fullProve(Input, "circuits/circuit_js/circuit.wasm","circuits/circuit_final.zkey");
+    const Input = {
+      leaf: "1",
+      path_elements: ["2", node9, node13],
+      path_index: ["0", "0", "0"],
+    };
+    const { proof, publicSignals } = await groth16.fullProve(
+      Input,
+      "circuits/circuit_js/circuit.wasm",
+      "circuits/circuit_final.zkey"
+    );
 
-        const calldata = await groth16.exportSolidityCallData(proof, publicSignals);
-    
-        const argv = calldata.replace(/["[\]\s]/g, "").split(',').map(x => BigInt(x).toString());
-    
-        const a = [argv[0], argv[1]];
-        const b = [[argv[2], argv[3]], [argv[4], argv[5]]];
-        const c = [argv[6], argv[7]];
-        const input = argv.slice(8);
+    const calldata = await groth16.exportSolidityCallData(proof, publicSignals);
 
-        expect(await merkleTree.verify(a, b, c, input)).to.be.true;
+    const argv = calldata
+      .replace(/["[\]\s]/g, "")
+      .split(",")
+      .map((x) => BigInt(x).toString());
 
-        // [bonus] verify the second leaf with the inclusion proof
-    });
+    const a = [argv[0], argv[1]];
+    const b = [
+      [argv[2], argv[3]],
+      [argv[4], argv[5]],
+    ];
+    const c = [argv[6], argv[7]];
+    const input = argv.slice(8);
+
+    expect(await merkleTree.verify(a, b, c, input)).to.be.true;
+
+    // [bonus] verify the second leaf with the inclusion proof
+  });
 });
